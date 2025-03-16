9d42227bfa6341bd0f4e9e3d98862db1fe3fbd5c
Hanze-C <hanze2010@outlook.com>
2025-03-16 20:04:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Providers } from "./providers"
 import { FloatMenu } from "@/components/float-menu"
 
 export const metadata: Metadata = {
-  title: "MoeMail - 萌萌哒临时邮箱服务",
+  title: "MoeMail - 次元星光邮件服务系统",
   description: "安全、快速、一次性的临时邮箱地址，保护您的隐私，远离垃圾邮件。支持即时收件，到期自动失效。",
   keywords: [
     "临时邮箱",
@@ -41,14 +41,14 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "zh_CN",
-    url: "https://moemail.app",
-    title: "MoeMail - 萌萌哒临时邮箱服务",
+    url: "https://mail.hanze.icu",
+    title: "MoeMail - 次元星光邮件服务系统",
     description: "安全、快速、一次性的临时邮箱地址，保护您的隐私，远离垃圾邮件。支持即时收件，到期自动失效。",
     siteName: "MoeMail",
   },
   twitter: {
     card: "summary_large_image",
-    title: "MoeMail - 萌萌哒临时邮箱服务",
+    title: "MoeMail - 次元星光邮件服务系统",
     description: "安全、快速、一次性的临时邮箱地址，保护您的隐私，远离垃圾邮件。支持即时收件，到期自动失效。",
   },
   manifest: '/manifest.json',
